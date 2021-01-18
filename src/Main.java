c9a79f9e007b885069da8b6c030d54388ca62f9c
kseniabobkova <bobkova@healbe.com>
2021-01-18 22:38:16 +0300

    inicial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        double topup = 1132.76;
+        double balance = 100.26;
+        double bonus = (int) topup /100;
+        int limit = 1000;
+        double total;
+        if (topup > limit) {
+            total = balance + topup + bonus ;
+        } else {
+            total = balance + topup;
+        }
+
+        System.out.println(total);
+    }
+}

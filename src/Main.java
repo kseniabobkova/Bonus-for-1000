2c4b89cd529653748634d38bd1fe9d4da4dc2738
kseniabobkova <bobkova@healbe.com>
2021-01-18 22:55:54 +0300

    Java

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,11 @@ public class Main {
     public static void main(String[] args) {
         double topup = 1132.76;
         double balance = 100.26;
-        double bonus = (int) topup /100;
+        double bonus = (int) topup / 100;
         int limit = 1000;
         double total;
         if (topup > limit) {
-            total = balance + topup + bonus ;
+            total = balance + topup + bonus;
         } else {
             total = balance + topup;
         }
@@ -14,3 +14,4 @@ public class Main {
         System.out.println(total);
     }
 }
+git
